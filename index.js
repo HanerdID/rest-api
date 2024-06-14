a14846469706b41d9d76a0e56cb6009051c50027
Fikri Prasetya <fikriprasetya3@gmail.com>
2024-06-15 01:59:22 +0700

    eight

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import cors from 'cors';
-import express from 'express';
-import dotenv from 'dotenv';
-import UserRoute from './routes/UserRoute.js';
+import cors from "cors";
+import express from "express";
+import dotenv from "dotenv";
+import UserRoute from "./routes/UserRoute.js";
+
 dotenv.config();
 
 const app = express();
@@ -10,7 +11,6 @@ app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
 
+const port = process.env.PORT || 5000;
 
-app.listen(process.env.PORT, () =>
-  console.log("Server is running on port 5000")
-);
+app.listen(port, () => console.log(`Server is running on port ${port}`));
