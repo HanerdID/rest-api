08f6732b358aecc3ad09c7283d1aca831d091ef6
Fikri Prasetya <fikriprasetya3@gmail.com>
2024-06-15 01:11:02 +0700

    first push

---
+++ routes/UserRoute.js
@@ -0,0 +1,12 @@
+import express from "express";
+import {createUser, deleteUser, getUser, getUserById,updateUser} from "../controllers/UserController.js";
+
+const router = express.Router();
+
+router.get("/user", getUser);
+router.get("/user/:id", getUserById);
+router.post("/user", createUser);
+router.put("/user/:id", updateUser);
+router.delete("/user/:id", deleteUser);
+
+export default router;
