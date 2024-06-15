5cca765dc110ccb8a967e9e84d64915d08ac05d7
Fikri Prasetya <fikriprasetya3@gmail.com>
2024-06-16 03:21:43 +0700

    ...

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(UserRoute);
 app.use(AuthRoute);
 app.use(HangoutRoute);
 
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
